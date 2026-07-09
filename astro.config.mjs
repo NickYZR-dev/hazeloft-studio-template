43f8f04cdda6e63b75e9a478b57a97910b852aa0
Hazeloft <github@hazeloft.com>
2026-07-09 17:43:46 +0300

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   integrations: [
-    UnoCSS({
-      injectReset: true
-    }),
-  ],
-})
+  UnoCSS(), 
+  icon({svgoOptions: { multipass: true, plugins: [ {name: "preset-default", params: { overrides: { inlineStyles: { onlyMatchedOnce: false, }, removeDoctype: false, } } } ] } })],
+});
--- uno.config.ts
+++ uno.config.ts
@@ -1,20 +1,5 @@
-import { defineConfig, presetWind4, presetWebFonts } from 'unocss'
+import { defineConfig, presetWind4 } from 'unocss'
 
 export default defineConfig({
-  presets: [
-    presetWind4(),
-    presetWebFonts({
-      provider: 'google',
-      fonts: {
-        sans: 'Geist:400,500,600',
-        mono: 'Geist Mono:400,500',
-      },
-    }),
-  ],
-  theme: {
-    colors: {
-      background: 'var(--background)',
-      foreground: 'var(--foreground)',
-    },
-  },
+  presets: [ presetWind4()],
 })
